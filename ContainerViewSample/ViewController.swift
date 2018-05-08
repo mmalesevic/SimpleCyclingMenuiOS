29e64f632089638dd8d79982f9977877e6023d9f
Matej Malesevic <matej.malesevic@gmail.com>
2018-05-08 16:57:29 +0200

    Initial Commit

---
+++ ContainerViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewSample
+//
+//  Created by Matej Malesevic on 08.05.18.
+//  Copyright © 2018 Approppo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
